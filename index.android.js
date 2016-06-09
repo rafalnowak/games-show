2d52aa965684376f3612e06bf029974798001ec1
Rafał Nowak <rafal.nowak1@gmail.com>
2016-06-10 01:02:38 +0200

    Game element component.

--- index.android.js
+++ index.android.js
@@ -1,24 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+class GameElement extends Component {
+  render() {
+    var game = {
+      name: 'Some game',
+      platform: 'PS4',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+    };
+    return(
+      <View style={styles.container}>
+        <Text>{game.name}</Text>
+        <Text>{game.platform}</Text>
+        <Image
+          source={{uri: game.thumbnail}}
+          style={styles.gameThumbnail}/>
+      </View>
+    );
+  }
+}
+
 class GamesShow extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Games quick library
-        </Text>
+        <GameElement/>
+        <GameElement/>
       </View>
     );
   }
@@ -30,6 +43,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  gameThumbnail: {
+    width: 53,
+    height: 81,
   },
   welcome: {
     fontSize: 20,
