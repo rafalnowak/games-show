dd1d99cec36d281eb295385c80c757e388cc8afc
Rafał Nowak <rafal.nowak1@gmail.com>
2016-06-10 12:05:04 +0200

    Simple games list skeleton.

--- GameElement.js
+++ GameElement.js
@@ -6,20 +6,39 @@ import {
   View
 } from 'react-native';
 
+var games = [
+  {
+    name: 'Stellaris',
+    platform: 'PC',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'The Witcher 3',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'GTA V',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  }
+];
+
 export class GameElement extends Component {
   render() {
-    var game = {
-      name: 'Some game',
-      platform: 'PS4',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
-    };
+    return this.renderGame(this.props.game);
+  }
+
+  renderGame(game) {
     return(
       <View style={styles.container}>
-        <Text>{game.name}</Text>
-        <Text>{game.platform}</Text>
         <Image
           source={{uri: game.thumbnail}}
           style={styles.gameThumbnail}/>
+        <View style={styles.rightContainer}>
+          <Text style={styles.name}>{game.name}</Text>
+          <Text style={styles.platform}>{game.platform}</Text>
+        </View>
       </View>
     );
   }
@@ -28,12 +47,25 @@ export class GameElement extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  rightContainer: {
+    flex: 1
+  },
+  name: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  platform: {
+    textAlign: 'center',
+  },
   gameThumbnail: {
     width: 53,
     height: 81,
-  }
+  },
+
 });
--- index.android.js
+++ index.android.js
@@ -1,33 +1,131 @@
 import React, { Component } from 'react';
 
 import {
+  Alert,
   AppRegistry,
+  ListView,
   StyleSheet,
+  Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 
 import { GameElement } from "./GameElement"
 
+var gamesMock = [
+  {
+    name: 'Stellaris',
+    platform: 'PC',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'The Witcher 3',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'GTA V',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'Hearts of Iron IV',
+    platform: 'PC',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'Metal Gear Solid V',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'The Journey',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'Something something',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'Mortal Kombat X',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  },
+  {
+    name: 'The Last Of Us',
+    platform: 'PS4',
+    thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Harry_Whittier_Frees_-_What%27s_Delaying_My_Dinner.jpg'
+  }
+];
+
 class GamesShow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    //todo: fetch data from igdb api, add search bar
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(gamesMock),
+      loaded: true
+    });
+  }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderGame}/>
+    );
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <GameElement/>
-        <GameElement/>
-        <GameElement/>
+        <Text style={styles.loading}>
+          Loading...
+        </Text>
       </View>
     );
+  }
+
+  renderGame(game) {
+    return (
+      <TouchableHighlight onPress={() => Alert.alert(
+            'Game details',
+            game.name + ' - ' + game.platform,
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed!')},
+            ]
+          )}>
+        <View>
+          <GameElement game={game}/>
+        </View>
+      </TouchableHighlight>);
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  loading: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
